1f4a1f9766779267a26ed7ca162ce74ec2526180
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-24 19:28:08 -0500

    Add files via upload

---
+++ herencia/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Humano humano = new Humano();
+            Console.WriteLine("soy humano:");
+            humano.Respira();
+
+            Gorila gorila = new Gorila();
+            Console.WriteLine("soy Gorila:");
+            gorila.SoyNegro();
+            gorila.Comer(); 
+          
+            Caballo caballo = new Caballo();
+            Console.WriteLine("soy un caballo :");
+            caballo.CorroRapido();
+            caballo.TienenHijos();
+
+            niños niño = new niños();
+            niño.lloro();
+            niño.Pienso();
+
+            Object obj = new object();
+            Console.WriteLine(obj.GetType().ToString()); 
+        }
+    }
+
+    class Mamiferos:Object
+    {
+        public void Respira()
+        {
+            Console.WriteLine("Respiro");
+        }
+        public void TienenHijos()
+        {
+            Console.WriteLine("Puedo tener hijos");
+        }
+        public void Comer()
+        {
+            Console.WriteLine("Puedo comer");
+        }
+
+    }
+    class Humano:Mamiferos
+    {
+        public void Pienso()
+        {
+            Console.WriteLine("Puedo pensar");
+        }
+    }
+    class Gorila:Mamiferos
+    {
+        public void SoyNegro()
+        {
+            Console.WriteLine("Soy de color negro");
+        }
+    }
+
+    class Caballo:Mamiferos
+    {
+        public void CorroRapido()
+        {
+            Console.WriteLine("Puedo correr rapido");
+        }
+    }
+    class niños : Humano
+    {
+        public void lloro()
+        {
+            Console.WriteLine("Llloro mucho");
+
+        }
+    } 
+}
